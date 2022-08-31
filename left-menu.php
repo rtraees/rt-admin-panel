a84421a01276f7a7557aea626e34b2b6a5bb49a6
rtraees <rtraees@gmail.com>
2022-08-31 19:14:30 +0500

    left menu setup

--- left-menu.php
+++ left-menu.php
@@ -11,22 +11,22 @@
                     <span class="menu-title">Home</span>
                     <span class="menu-arrow float-end badge">></span>
                 </a>
-                <ul style="list-style:none;padding:0px; background-color:#c5c5c5; ;">
-                    <li class="rt-left-menu-li">
+                <ul>
+                    <li>
                         <a href="#" class="rt-left-menu-link">
-                            <i class="bi-android2" style="color:#E5E5E5"></i>
-                            
-                            <span class="menu-title" style="color:#01777a">Home</span>
-                            <span class="menu-arrow float-end badge">></span>
+                            <i class="bi-android2"></i>
+                            <span class="menu-title">Home</span>
+                            <span class="menu-arrow float-end badge"></span>
                         </a>
                     </li>
                 </ul>
             </li>
             <li class="rt-left-menu-li">
-                <a href="Icons.html" class="rt-left-menu-link">
-                    <i class="menu-ico bi bi-airplane float-start"></i>
-                    <span class="menu-title">Icon's Page</span>
-                    <span class="menu-arrow float-end badge">T</span>
+                <a href="#" class="rt-left-menu-link">
+                    <i class="bi-android2 float-start"></i>
+                    
+                    <span class="menu-title">Home</span>
+                    <span class="menu-arrow float-end badge">></span>
                 </a>
             </li>
             <li class="rt-left-menu-li">
